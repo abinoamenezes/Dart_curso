dc0872bc0b34533ef66a7d797f84724df4ef20cf
Abinoã Menezes <abinoamenezes89@gmail.com>
2022-11-30 15:50:04 -0300

    .

--- fundamentos/constants.dart
+++ fundamentos/constants.dart
@@ -3,9 +3,11 @@ import 'dart:io';
 void main() {
   //calculando area da circuferencia
   stdout.write('Digite um valor: ');
-  final PI = 3.14;
+  const PI = 3.14;
+
   final String x = stdin.readLineSync()!;
+
   final raio = double.parse(x);
   final area = PI * raio * raio;
-  print('A área da circuferência é' + area.toString());
+  print('A área da circuferência é ' + area.toString());
 }
