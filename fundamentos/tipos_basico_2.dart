04b3db432a615cb1e30de80dee4544acfa01dcab
Abinoã Menezes <abinoamenezes89@gmail.com>
2022-11-30 20:11:15 -0300

    Update tipos_basico_2.dart

---
+++ fundamentos/tipos_basico_2.dart
@@ -0,0 +1,17 @@
+main() {
+  //lista
+  List nomes = ['carlos', 'Miguel', 'Vitor'];
+  print(nomes.elementAt(1));
+  print(nomes[0]);
+  print(nomes.length);
+
+  //mapa(dicionario)
+
+  Map telefones = {'Abino': '98986-9414', 'Fernanda': '7623-5645'};
+  print(telefones);
+  print(telefones['Abino']);
+  print(telefones.length);
+  print(telefones.keys);
+  print(telefones.values);
+  print(telefones.entries);
+}
