55f2d24ab607864984fdae1866091c0727a839db
Abinoã Menezes <abinoamenezes89@gmail.com>
2023-01-07 17:50:11 -0300

    Update class_dat.dart

--- POO/class_dat.dart
+++ POO/class_dat.dart
@@ -3,10 +3,16 @@ class Data {
   var mes;
   var ano;
 
+//construtor padrão
   Data([this.dia = 0, this.mes = 00, this.ano = 0000]) {}
 
   String obterDataFormatada() {
     return '$dia/$mes/$ano';
+  }
+
+  Data.ultimoDia(int this.ano) {
+    dia = 31;
+    mes = 12;
   }
 }
 
@@ -14,4 +20,7 @@ main() {
   Data dataAniversario = Data(04, 02, 2002);
 
   print('A data de aniversario é ${dataAniversario.obterDataFormatada()}');
+
+  Data ultimoDiaAno = Data.ultimoDia(2022);
+  print(ultimoDiaAno.obterDataFormatada());
 }
